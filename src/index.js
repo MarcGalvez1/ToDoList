f0d3eb29dfe5d57e1367d5b1c86e9af1cae84c36
MarcGalvez1 <marc.galvez83@gmail.com>
2024-01-17 00:48:01 -0500

    Created the base of the nav bar and the sliding menu

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import _ from "lodash";
 
-function component() {
-  const element = document.createElement("div");
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  return element;
-}
-
-document.body.appendChild(component());
+const test = document.createElement("h1");
+test.innerText = "TESTING TESTING";
+document.body.appendChild(test);
---
+++ src/nav.js
@@ -0,0 +1,7 @@
+function navBar() {
+  const nav = document.createElement("nav");
+
+  return nav;
+}
+
+export default navBar;
