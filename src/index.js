35855d585f10c19ae0b8f1896c7e59f8122a39f2
MarcGalvez1 <marc.galvez83@gmail.com>
2024-01-22 01:23:01 -0500

    created the nav bar with js need to create the event listeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import _ from "lodash";
+import navBar from "./nav";
+
+const content = document.getElementById("content");
+
+content.appendChild(navBar());
--- src/nav.js
+++ src/nav.js
@@ -1,7 +1,52 @@
 function navBar() {
-  const nav = document.createElement("nav");
+  // Outer most nav container
+  const navContainer = document.createElement("nav");
+  navContainer.classList.add(
+    "navbar",
+    "navbar-light",
+    "bg-light",
+    "row",
+    "bg-dark"
+  );
 
-  return nav;
+  // Contains all inner elements
+  const navContent = document.createElement("div");
+  navContent.classList.add("container-fluid");
+
+  // Icon container
+  const iconContainer = document.createElement("icon");
+  iconContainer.classList.add("icon");
+
+  // Creates the 3 icon bars
+  for (let i = 1; i <= 3; i++) {
+    const IconBar = document.createElement("div");
+    IconBar.classList.add("bar" + i);
+    iconContainer.appendChild(IconBar);
+  }
+
+  // Logo
+  const logo = document.createElement("img");
+  logo.classList.add("d-inline-block", "align-text-top", "custom-logo");
+  logo.src = "./images/logo.png";
+
+  // Title
+  const title = document.createElement("h1");
+  title.classList.add(
+    "display-1",
+    "mx-auto",
+    "dblock",
+    "text-center",
+    "text-light"
+  );
+  title.innerText = "To-Do List";
+
+  navContent.appendChild(iconContainer);
+  navContent.appendChild(logo);
+  navContent.appendChild(title);
+
+  navContainer.appendChild(navContent);
+
+  return navContainer;
 }
 
 export default navBar;
