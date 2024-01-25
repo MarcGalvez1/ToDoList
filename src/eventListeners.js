35f2b007b69986f5ad957c4df4286cf1b98d4074
MarcGalvez1 <marc.galvez83@gmail.com>
2024-01-25 16:14:31 -0500

    Documented the eventListeners.js for readability

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -1,15 +1,18 @@
 function eventListeners() {
   document.addEventListener("DOMContentLoaded", () => {
+    //Ensures that the DOM is loaded before activating the event listeners
     const sideMenuSlide = document.getElementById("menu");
     const todosSlide = document.getElementById("todos");
     const iconToggle = document.getElementById("icon");
 
     iconToggle.addEventListener("click", () => {
+      //Allows the menu to slide in and out when the icon is clicked
       sideMenuSlide.classList.toggle("slide-in-out");
       todosSlide.classList.toggle("custom-todos");
     });
 
     window.addEventListener("resize", function () {
+      // Ensures that the menu is in the correct position when the page is resized
       if (window.innerWidth > 768) {
         sideMenuSlide.classList.remove("slide-in-out");
         todosSlide.classList.remove("custom-todos"); // Remove "custom-todos" when menu is visible
@@ -20,6 +23,7 @@ function eventListeners() {
     });
 
     window.addEventListener("load", function () {
+      // Ensures that the menu is in the correct position when the page is loaded
       if (window.innerWidth > 768) {
         sideMenuSlide.classList.remove("slide-in-out");
       } else {
